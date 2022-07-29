c013460aab384e43ff86cf0154806290a7b4d178
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-29 17:04:07 +0900

    QueryClientProvider 컴포넌트에 공급
    Co-authored-by: 전인호 <Kim-Haul@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Suspense fallback="로딩중입니다...">
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </Suspense>,
 );
