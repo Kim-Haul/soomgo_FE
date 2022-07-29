31fd1f84fc1410e7a1cf966d369eb2ce3afb9093
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-30 01:20:54 +0900

    글로벌 스타일 및 폰트 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { GlobalStyles } from './styles/GlobalStyle';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -7,6 +8,7 @@ import Signup from './pages/Signup';
 const App = () => {
   return (
     <>
+      <GlobalStyles />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
