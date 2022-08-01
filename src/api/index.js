faf82907c4489b25c9968a5762a39416609f4422
Sooyoung Yoon <ayoooo240@gmail.com>
2022-08-01 20:17:14 +0900

    api 추가

--- src/api/index.js
+++ src/api/index.js
@@ -15,15 +15,34 @@ api.interceptors.request.use((config) => {
 });
 
 const apis = {
-  // auth
+  // auth & mypage
   signup: (data) => api.post('/signup', data),
   login: (data) => api.post('/login', data),
+  toggleRole: () => api.put('/role'), // 고수 유저 간 전환
   getAuth: () => api.get('/auth'),
-  // posts
-  // getDetailData: () => api.get('posts'),
-  // getCommentsData: () => api.get('comments'),
-  // addComment: (data) => api.post('comments', data),
-  // deleteComment: (id) => api.delete('comments', id)
+  editAuth: (data) => api.put('/auth', data),
+  getMypost: () => api.get('/myposts'),
+
+  // post
+  getDetailPost: (postId) => api.get(`posts/${postId}`),
+  addPost: (data) => api.post('posts', data),
+  editPost: (postId, data) => api.put(`posts/${postId}`, data),
+  deletePost: (postId) => api.delete(`posts/${postId}`),
+
+  // comments
+  getCommentsData: (postId) => api.get(`comments/${postId}`),
+  addComment: (postId, data) => api.post(`comments/${postId}`, data),
+  editComment: (postId, data) => api.put(`comments/${postId}`, data),
+  deleteComment: (commentId) => api.delete(`comments/${commentId}`),
+
+  // like
+  addLike: (postId) => api.post(`likes/${postId}`),
+  removeLike: (postId) => api.delete(`likes/${postId}`),
+
+  // bookmark
+  getBookmarkedPosts: () => api.get('mybookmark'),
+  addBookmark: (postId) => api.post(`bookmark/${postId}`),
+  removeBookmark: (postId) => api.delete(`bookmark/${postId}`),
 };
 
 export default apis;
