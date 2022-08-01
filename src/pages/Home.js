50fd9e8478b66086e054ca756cace62befc0a596
Sooyoung Yoon <ayoooo240@gmail.com>
2022-08-01 10:37:14 +0900

    메인 페이지 카테고리 리스트 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import apis from '../api';
 import MainSlider from '../components/MainSlider';
+import { mainCategories } from '../data';
 
 const Home = () => {
   const getAuthInfo = async () => {
@@ -15,6 +18,14 @@ const Home = () => {
   return (
     <>
       <MainSlider />
+      <MainCategoryList>
+        {mainCategories.map((cat) => (
+          <li key={cat.id}>
+            <img src={cat.img} alt="" />
+            {cat.text}
+          </li>
+        ))}
+      </MainCategoryList>
       <div>
         <button onClick={getAuthInfo}>auth 불러오기</button>
       </div>
@@ -23,3 +34,28 @@ const Home = () => {
 };
 
 export default Home;
+
+const MainCategoryList = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  margin: 32px 0 64px;
+  padding: 16px;
+  li {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 96px;
+    height: 96px;
+    border-radius: 20px;
+    font-weight: 500;
+    transition: all 0.2s ease;
+    img {
+      margin-bottom: 4px;
+    }
+    &:hover {
+      background: #fafafa;
+      transform: translateY(-4px);
+    }
+  }
+`;
